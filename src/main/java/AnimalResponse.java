fda1ece2353a64b5b07d4c0b1af94d7337779892
okamura <okmr106@gmail.com>
2022-07-01 12:51:38 +0900

    PokemonItemのフィールドに"no"を追加。

---
+++ src/main/java/AnimalResponse.java
@@ -0,0 +1,9 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class AnimalResponse {
+    @JsonProperty("2")
+    public List<String> two;
+}
